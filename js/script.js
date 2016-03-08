2c36cd2432f7b8fb3f141c43134a856d1fa18ce7
Diana Duong <dduong013@gmail.com>
2016-03-08 14:41:25 -0600

    initial commit

--- js/script.js
+++ js/script.js
@@ -1,17 +1,22 @@
+var input = prompt("Please type a number between 1 and 100.");
+var number = parseInt(input);
 
-		for (var i = 1; i < 101; i++) {
 
-			if (i % 3 == 0 && i % 5 == 0) { //divisble by 3 and 5 = fizzbuzz
-				document.write( "fizzbuzz" + "<br>" );
-			} 
-			else if ( i % 5 == 0) { //divisble by 5 = buzz
-				document.write(  "buzz" + "<br>");
-			}
-			else if  ( i % 3 == 0 ) {  //divisble by 3 = fizz
-				document.write( "fizz" + "<br>"); 
-			}
-			else {
-				document.write(  i + "<br>");
-			}
-		}
+function fizzbuzz (number){
+    for(var i = 1; i <= number; i++) {
+        if (i % 3 == 0 && i % 5 == 0) {
+            document.write( "fizzbuzz" + "<br>" );
+        }
+        else if ( i % 5 == 0) {
+            document.write(  "buzz" + "<br>");
+        }
+        else if  ( i % 3 == 0 ) { 
+            document.write( "fizz" + "<br>");
+        }
+        else {
+            document.write(  i + "<br>");
+        }
+    }
+}
 
+console.log(fizzbuzz(number));
